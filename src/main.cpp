f7b4d2a34f3288432f76fc1ea9c2b39b91fec251
roeschm <michael.roesch93@gmail.com>
2024-11-05 22:48:41 +0100

    implemented game logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,33 @@
 
 bool gboGameState = 0;
 
-
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // init of buzzer 
   for (int i = 0; i < N_BUZZER; i++) {
     vInitBuzzerPin(gaptBuzzerList[i]);
   }
+
+  
 }
 
 void loop() {
   
+  // reset game state
+  
+
   if (0 == gboGameState)
   {
     // read all inputs
-    vReadFromInputPin(&gtBuzzerGreen);
-    vReadFromInputPin(&gtBuzzerBlue);
-    vReadFromInputPin(&gtBuzzerRed);
-    vReadFromInputPin(&gtBuzzerYellow);
-    vReadFromInputPin(&gtBuzzerWhite);
+    for (int i = 0; i < N_BUZZER; i++) {
+
+      // read the switch pin
+      vReadFromInputPin(gaptBuzzerList[i]);
+
+      if (true == boIsBuzzerPressed(gaptBuzzerList[i])) 
+      {
+        vWriteToLEDPin(gaptBuzzerList[i]);
+        gboGameState = 1;
+      }      
+    }
   }
-
-
-  // process all inputs
-  if (true == boIsBuzzerPressed(&gtBuzzerGreen)) {
-    vSetLEDState(&gtBuzzerGreen, true);
-  } else {
-    vSetLEDState(&gtBuzzerGreen, false);
-  }
-
-  // write output pins
-  vWriteToLEDPin(&gtBuzzerGreen);
-  vWriteToLEDPin(&gtBuzzerBlue);
-  vWriteToLEDPin(&gtBuzzerRed);
-  vWriteToLEDPin(&gtBuzzerYellow);
-  vWriteToLEDPin(&gtBuzzerWhite);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
